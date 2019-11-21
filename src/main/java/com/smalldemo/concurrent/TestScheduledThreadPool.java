6a0ef8cf49953176e5fc9b58062a6aa0dde2d6a2
mortalliao <mortalliaoyujian@163.com>
2019-11-22 00:24:05 +0800

    ThreadPool

---
+++ src/main/java/com/smalldemo/concurrent/TestScheduledThreadPool.java
@@ -0,0 +1,27 @@
+package com.smalldemo.concurrent;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+/**
+ * @author Jim
+ */
+public class TestScheduledThreadPool {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ScheduledExecutorService pool = Executors.newScheduledThreadPool(5);
+
+        for (int i = 0; i < 5; i++) {
+            Future<Integer> result = pool.schedule(new Callable<Integer>() {
+                @Override
+                public Integer call() throws Exception {
+                    int num = new Random().nextInt();
+                    System.out.println(Thread.currentThread().getName() + " : " + num);
+                    return num;
+                }
+            }, 1, TimeUnit.SECONDS);
+
+            System.out.println(result.get());
+        }
+    }
+}
