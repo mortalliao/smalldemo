300d3c154cfbf3f78a6471b5016f86284f2ef1b9
mortalliao <mortalliaoyujian@163.com>
2019-12-02 00:32:19 +0800

    java8

--- src/main/java/com/smalldemo/java8/LambdaTest.java
+++ src/main/java/com/smalldemo/java8/LambdaTest.java
@@ -90,7 +90,7 @@ public class LambdaTest {
 
     @Test
     public void test4() {
-        // 优化方式一:
+        // 优化方式一: 策略设计模式
         List<Employee> list = filterEmployee(this.employees, new FilterEmployeeByAge());
         for (Employee employee : list) {
             System.out.println(employee);
@@ -104,6 +104,41 @@ public class LambdaTest {
         }
     }
 
+    @Test
+    public void test5() {
+        // 优化方式二: 匿名内部类
+        List<Employee> list = filterEmployee(this.employees, new MyPredicate<Employee>() {
+            @Override
+            public boolean test(Employee employee) {
+                return employee.getSalary() <= 5000;
+            }
+        });
 
+        for (Employee employee : list) {
+            System.out.println(employee);
+        }
+    }
+
+    @Test
+    public void test6() {
+        // 优化方式三: Lambda表达式
+        List<Employee> list = filterEmployee(this.employees, (employee) -> employee.getSalary() <= 5000);
+        list.forEach(System.out::println);
+    }
+
+    @Test
+    public void test7() {
+        // 优化方式四: Stream API
+        employees.stream()
+                .filter(employee -> employee.getSalary() >= 5000)
+                .limit(2)
+                .forEach(System.out::println);
+
+        System.out.println("-----------------------");
+
+        employees.stream()
+                .map(Employee::getName)
+                .forEach(System.out::println);
+    }
 
 }
