cd778fd40650089e78df7f6154012dd6cbe61e19
mortalliao <mortalliaoyujian@163.com>
2019-12-07 10:23:16 +0800

    lock

--- src/main/java/com/smalldemo/concurrency/TestThread8Monitor.java
+++ src/main/java/com/smalldemo/concurrency/TestThread8Monitor.java
@@ -2,36 +2,48 @@ package com.smalldemo.concurrency;
 
 /**
  * @author Jim
- * <p>
+ * <pre>
  * 1. 两个普通同步方法, 两个线程, 标准打印, 打印? //one two
  * 2. 新增Thread.sleep() 给getOne(), 打印? //one two
  * 3. 新增普通方法 getThree(), 打印? // three one two
+ * 4. 两个普通同步方法, 两个Number对象, 打印?  // two one
+ * 5. 修改getOne()为静态同步方法, 打印? // two one
+ * 6. 修改两个方法均为静态同步方法, 一个Number对象? // one two
+ * 7. 一个是静态同步方法, 一个非静态同步方法, 两个Number对象? // two one
+ * 8. 两个静态同步方法, 两个Number对象?
  *
- *
+ * 关键:
+ * 1. 非静态方法的锁默认为 this, 静态方法的锁为 对应的Class 实例
+ * 2. 某一个时刻内, 只能有一个线程持有锁, 无论几个方法
+ * </pre>
  */
 public class TestThread8Monitor {
 
     public static void main(String[] args) {
-        Number number = new Number();
+        Number number1 = new Number();
+        Number number2 = new Number();
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                number.getOne();
+//                number1.getOne();
+                Number.getOne();
             }
         }).start();
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                number.getTwo();
+//                number1.getTwo();
+//                number2.getTwo();
+                Number.getTwo();
             }
         }).start();
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                number.getThree();
+                number1.getThree();
             }
         }).start();
     }
@@ -39,7 +51,7 @@ public class TestThread8Monitor {
 
 class Number {
 
-    public synchronized void getOne() {
+    public static synchronized void getOne() {
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
@@ -48,7 +60,7 @@ class Number {
         System.out.println("one");
     }
 
-    public synchronized void getTwo() {
+    public static synchronized void getTwo() {
         System.out.println("two");
     }
 
